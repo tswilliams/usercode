e86b6dc4afed44977894f8d2492e137c09d41250
tsw <>
2011-07-06 17:11:07 +0000

    File required to add tsw::Event branch to the NTuples. Copied from the StoppedHSCP tupilisation code.

---
+++ BstdZee/BstdZeeNTupler/src/LinkDef.h
@@ -0,0 +1,13 @@
+#include <vector>
+#include "boost/cstdint.hpp"
+#include "NTupler/BstdZeeNTupler/interface/tswEvent.h"
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class std::vector<ULong64_t>;
+#pragma link C++ class tsw::Event;
+#endif
