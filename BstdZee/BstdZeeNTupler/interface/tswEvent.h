e1a8bb40f199e3d269e0443565d0243a5516ed38
tsw <>
2011-07-15 17:14:48 +0000

    Member variables and methods added to tsw::Event class for storing muon information.

--- BstdZee/BstdZeeNTupler/interface/tswEvent.h
+++ BstdZee/BstdZeeNTupler/interface/tswEvent.h
@@ -6,8 +6,12 @@
 
 #include <vector>
 
+#include "NTupler/BstdZeeNTupler/interface/tswMuStruct.h"
+//#include "BstdZeeFirst/Analyser/interface/tswEventHelper.h"
+
 namespace tsw{
 	class Event : public TObject {
+		friend class EventHelper;
 		public:
 			Event();
 			~Event();
@@ -15,6 +19,7 @@ namespace tsw{
 			//Methods to set information ...
 			void SetBasicEventInformation(unsigned int runNumber, unsigned int lumiSection, unsigned int eventNumber);
 			void PrintBasicEventInformation();
+			void AddNormMuon(tsw::MuStruct* theMuon);
 
 		private:
 			//General event information ...
@@ -22,7 +27,39 @@ namespace tsw{
 			unsigned int lumiSec_;
 			unsigned int evtNum_;
 
-			ClassDef(tsw::Event,2);
+			//Information about the standard muons ...
+			std::vector<ROOT::Math::XYZTVector> normMuons_p4_;
+			std::vector<Int_t>    normMuons_charge_;
+			std::vector<Bool_t>   normMuons_isGlobalMuon_;
+			std::vector<Bool_t>   normMuons_isTrackerMuon_;
+			std::vector<Bool_t>   normMuons_isStandAloneMuon_;
+			std::vector<Int_t>    normMuons_numMatchedMuonStns_;
+			std::vector<Double_t> normMuons_isolR03_sumPt_;
+
+			std::vector<Bool_t>   normMuons_globTrk_exists_;
+			std::vector<Double_t> normMuons_globTrk_pT_;
+			std::vector<Double_t> normMuons_globTrk_eta_;
+			std::vector<Double_t> normMuons_globTrk_phi_;
+			std::vector<Int_t>    normMuons_globTrk_charge_;
+			std::vector<Int_t>    normMuons_globTrk_numberOfValidMuonHits_;
+			std::vector<Double_t> normMuons_globTrk_normalisedChi2_;
+
+			std::vector<Bool_t>   normMuons_inTrk_exists_;
+			std::vector<Double_t> normMuons_inTrk_pT_;
+			std::vector<Double_t> normMuons_inTrk_eta_;
+			std::vector<Double_t> normMuons_inTrk_phi_;
+			std::vector<Int_t>    normMuons_inTrk_charge_;
+			std::vector<Int_t>    normMuons_inTrk_numValidPixHits_;
+			std::vector<Int_t>    normMuons_inTrk_numValidTrkrHits_;
+			std::vector<Double_t> normMuons_inTrk_dxyVsOrigin_;
+
+			std::vector<Bool_t>   normMuons_outTrk_exists_;
+			std::vector<Double_t> normMuons_outTrk_pT_;
+			std::vector<Double_t> normMuons_outTrk_eta_;
+			std::vector<Double_t> normMuons_outTrk_phi_;
+			std::vector<Int_t>    normMuons_outTrk_charge_;
+
+			ClassDef(tsw::Event,5);
 	};
 }
 
