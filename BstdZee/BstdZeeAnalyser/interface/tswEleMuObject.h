77d73bc5bd8c07c67c332b7658866b85a3f9fe1a
tsw <>
2011-07-15 17:36:28 +0000

    Header file containing definition/implementation of tsw::EleMuObject.h class that is used to store the information of electron-muon objects. (Electron-muon objects are used in ttbar,tW and WW background estimation.)

---
+++ BstdZee/BstdZeeAnalyser/interface/tswEleMuObject.h
@@ -0,0 +1,55 @@
+#ifndef tswEleMuObject_h
+#define tswEleMuObject_h
+
+#include "BstdZeeFirst/Analyser/interface/tswMuon.h"
+
+namespace tsw{
+	class EleMuObject{
+		private:
+			tsw::HEEPEle ele_;
+			tsw::Muon muon_;
+			TLorentzVector p4_;
+
+		public:
+			// CTORs and DTOR ...
+			EleMuObject() :
+				ele_(), muon_(), p4_()
+				{}
+			EleMuObject(tsw::HEEPEle ele, tsw::Muon muon):
+				ele_(ele), muon_(muon), p4_( muon_.p4()+ele_.p4() )
+				{}
+			~EleMuObject(){}
+
+			// Methods to access the electron and muon ...
+			tsw::HEEPEle* GetElectron(){
+				return &ele_;}
+			tsw::Muon* GetMuon(){
+				return &muon_;}
+
+			// Methods to access kinematic variables of the ele-mu object ...
+			int charge(){
+				return ( muon_.charge()+ele_.charge() );}
+			TLorentzVector p4(){
+				return p4_;}
+			double p(){
+				return p4().P();}
+			double pT(){
+				return p4().Pt();}
+			double eta(){
+				return p4().Eta();}
+			double phi(){
+				return p4().Phi();}
+			double mass(){
+				return p4().M();}
+
+			bool isInZMassRange(){
+				if( mass()<120 && mass()>60)
+					return true;
+				else
+					return false;
+			}
+
+	};
+}
+
+#endif
