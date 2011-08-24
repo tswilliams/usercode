8af25aa7f6b529b7b4db040074e52fc19a0669a7
tsw <>
2011-08-24 10:48:10 +0000

    Lines added for linking libraries of vector<vector<XXX>> (where XXX=float & bool) to ensure error-free read out of normEles_SC_recHits_* branches.

--- BstdZee/BstdZeeNTupler/src/LinkDef.h
+++ BstdZee/BstdZeeNTupler/src/LinkDef.h
@@ -9,5 +9,7 @@
 #pragma link off all functions;
 
 #pragma link C++ class std::vector<ULong64_t>;
+#pragma link C++ class std::vector< std::vector<float> >;
+#pragma link C++ class std::vector< std::vector<bool> >;
 #pragma link C++ class tsw::Event;
 #endif
