bb7bdc76032680b42b9a2d20198303a4e66d527d
tsw <>
2011-08-30 00:34:44 +0000

    *** empty log message ***

--- BstdZee/BstdZeeAnalyser/interface/tswDiEleDistns.h
+++ BstdZee/BstdZeeAnalyser/interface/tswDiEleDistns.h
@@ -9,7 +9,7 @@ namespace tsw{
 			~DiEleDistns();
 
 			//Method to fill the histograms
-			void FillHistos(tsw::HEEPDiEle theDiEle, const Double_t dieleWeight);
+			void FillHistos(tsw::HEEPDiEle theDiEle, const Double_t dieleWeight, const bool );
 
 			//Method for setting the line attributes for the histograms ...
 			void SetHistAttributes(Int_t lineCol, Int_t lineStyle);
@@ -30,6 +30,8 @@ namespace tsw{
 			TH1D* diEleHist_deltaEta_;
 			TH1D* diEleHist_deltaPhi_;
 			TH1D* diEleHist_deltaR_;
+			TH1D* diEleHist_EtAminEtB_;
+			TH1D* diEleHist_EtAOverEtB_;
 			TH1D* diEleHist_modTrkIso_;
 			TH1D* diEleHist_dRleq03modTrkIso_;
 			TH1D* diEleHist_dRleq03modTrkIsoNoCtfTrk_;
@@ -41,6 +43,12 @@ namespace tsw{
 			TH1D* eleAHist_modTrkIso_;
 			TH1D* eleAHist_dRleq03modTrkIso_;
 			TH1D* eleAHist_dRleq03modTrkIsoNoCtfTrk_;
+			TH1D* eleAHist_modEmHad1Iso_;
+			TH1D* eleAHist_modEmHad1IsoOverCutThr_;
+			TH1D* eleAHist_modHad1IsoOverModEmHad1_;
+			TH1D* eleAHist_Had1Iso_;
+			TH1D* eleAHist_modHad1Iso_;
+			TH1D* eleAHist_modHad1IsoOverHad1_;
 
 			TH1D* eleBHist_et_;
 			TH1D* eleBHist_energy_;
@@ -49,6 +57,12 @@ namespace tsw{
 			TH1D* eleBHist_modTrkIso_;
 			TH1D* eleBHist_dRleq03modTrkIso_;
 			TH1D* eleBHist_dRleq03modTrkIsoNoCtfTrk_;
+			TH1D* eleBHist_modEmHad1Iso_;
+			TH1D* eleBHist_modEmHad1IsoOverCutThr_;
+			TH1D* eleBHist_modHad1IsoOverModEmHad1_;
+			TH1D* eleBHist_Had1Iso_;
+			TH1D* eleBHist_modHad1Iso_;
+			TH1D* eleBHist_modHad1IsoOverHad1_;
 	};
 
 	/////////////////////
@@ -77,7 +91,6 @@ namespace tsw{
 		diEleHist_invMass_     = new TH1D(hNamePrefix + "invMass",   "Di-electron invariant mass distribution  (" + str_eleType_GSFeles_cutsPhrase + "); M_{ee} /GeVc^{-2};" + str_NoOfDiEles + " per GeV", hNBins_mass, 50.0, 130.0);
 		Double_t hMax_DiEleEt = 1200.0;
 		diEleHist_sumEt_       = new TH1D(hNamePrefix + "sumEt",     "Di-electron p_{T} distribution  (" + str_eleType_GSFeles_cutsPhrase + "); p_{T, ee} /GeVc^{-1};" + str_NoOfDiEles, hNBins_pt, 0.0, hMax_pt);
-
 		Double_t hBinLims_ptLog[121]; hBinLims_ptLog[0] = 0.1;
 		for(unsigned int i=1; i<121; i++){
 			hBinLims_ptLog[i] = pow(hMax_pt/hBinLims_ptLog[0], 1.0/120.0)*hBinLims_ptLog[i-1];
@@ -87,6 +100,8 @@ namespace tsw{
 		diEleHist_deltaEta_    = new TH1D(hNamePrefix + "deltaEta",  "#Delta#eta_{ee} distribution  (" + str_eleType_GSFeles_cutsPhrase + "); #Delta#eta_{ee};" + str_NoOfDiEles, 50, -5.0, 5.0);
 		diEleHist_deltaPhi_    = new TH1D(hNamePrefix + "deltaPhi",  "#Delta#phi_{ee} distribution  (" + str_eleType_GSFeles_cutsPhrase + "); #Delta#phi_{ee};" + str_NoOfDiEles, 30, -3.15, 3.15);
 		diEleHist_deltaR_      = new TH1D(hNamePrefix + "deltaR",  "#Delta{}R_{ee} distribution  (" + str_eleType_GSFeles_cutsPhrase + "); #Delta{}R_{ee};" + str_NoOfDiEles, 50, 0.0, 5.0);
+		diEleHist_EtAminEtB_   = new TH1D(hNamePrefix + "EtAminEtB",  "E_{T}^{A}-E_{T}^{B} distribution  (" + str_eleType_GSFeles_cutsPhrase + "); E_{T}^{A}-E_{T}^{B} /GeV;" + str_NoOfDiEles, 120, 0.0, 1200.0);
+		diEleHist_EtAOverEtB_  = new TH1D(hNamePrefix + "EtAOverEtB", "E_{T}^{A}/E_{T}^{B} distribution  (" + str_eleType_GSFeles_cutsPhrase + "); E_{T}^{A}/E_{T}^{B};" + str_NoOfDiEles, 200, 0.0, 20.0);
 
 		//Initialise the single ele histos ...
 		Int_t hNBins_eleEt = hNBins_pt; Double_t hMin_eleEt = 0.0; Double_t hMax_eleEt = hMax_pt/2.0;
@@ -116,6 +131,22 @@ namespace tsw{
 		eleBHist_dRleq03modTrkIso_ = new TH1D(hNamePrefix + "eleB_dRleq03modTrkIso",  "eleB modTrkIso if modified distribution (" + str_eleType_GSFeles_cutsPhrase + "); eleB #Sigma p_{T}^{trk} if #Delta R_{ee}<0.3;" + str_NoOfEles, 80, -300.0, +300.0);
 		eleBHist_dRleq03modTrkIsoNoCtfTrk_ = new TH1D(hNamePrefix + "eleB_dRleq03modTrkIsoNoCtfTrk",  "eleB modTrkIso if (modified AND no eleA CTF) distribution (" + str_eleType_GSFeles_cutsPhrase + "); eleB #Sigma p_{T}^{trk} if #Delta R_{ee}<0.3 and eleA hasn't got closest CTF track;" + str_NoOfEles, 80, -300.0, +300.0);
 
+		//Initialise the EmHad1 isolation histograms ...
+		eleAHist_modEmHad1Iso_ = new TH1D(hNamePrefix + "eleA_modEmHad1Iso",  "eleA modEmHad1Iso distribution (" + str_eleType_GSFeles_cutsPhrase + "); eleA #Sigma E_{T}^{EmHad1};" + str_NoOfEles, 200, -100.0, +100.0);
+		eleAHist_modEmHad1IsoOverCutThr_ = new TH1D(hNamePrefix + "eleA_modEmHad1IsoOverCutThr",  "eleA modEmHad1Iso/cut distribution (" + str_eleType_GSFeles_cutsPhrase + "); eleA #Sigma E_{T}^{EmHad1} / Cut thr;" + str_NoOfEles, 80, -8.0, +8.0);
+		eleAHist_modHad1IsoOverModEmHad1_ = new TH1D(hNamePrefix + "eleA_modHad1IsoOverModEmHad1",  "eleA Had1Iso/modEmHad1Iso distribution (" + str_eleType_GSFeles_cutsPhrase + "); eleA #Sigma E_{T}^{Had1} / #Sigma E_{T}^{EmHad1};" + str_NoOfEles, 50, 0.0, +1.0);
+		eleBHist_modEmHad1Iso_ = new TH1D(hNamePrefix + "eleB_modEmHad1Iso",  "eleB modEmHad1Iso distribution (" + str_eleType_GSFeles_cutsPhrase + "); eleB #Sigma E_{T}^{EmHad1};" + str_NoOfEles, 200, -100.0, +100.0);
+		eleBHist_modEmHad1IsoOverCutThr_ = new TH1D(hNamePrefix + "eleB_modEmHad1IsoOverCutThr",  "eleB modEmHad1Iso/cut distribution (" + str_eleType_GSFeles_cutsPhrase + "); eleB #Sigma E_{T}^{EmHad1} / Cut thr;" + str_NoOfEles, 80, -8.0, +8.0);
+		eleBHist_modHad1IsoOverModEmHad1_ = new TH1D(hNamePrefix + "eleB_modHad1IsoOverModEmHad1",  "eleB Had1Iso/modEmHad1Iso distribution (" + str_eleType_GSFeles_cutsPhrase + "); eleB #Sigma E_{T}^{Had1} / #Sigma E_{T}^{EmHad1};" + str_NoOfEles, 50, 0.0, +1.0);
+
+		//Initialise the Had1 isolation histograms ...
+		eleAHist_Had1Iso_ = new TH1D(hNamePrefix + "eleA_Had1Iso",  "eleA Had1Iso distribution (" + str_eleType_GSFeles_cutsPhrase + "); eleA #Sigma E_{T}^{Had1};" + str_NoOfEles, 200, -10.0, +10.0);
+		eleBHist_Had1Iso_ = new TH1D(hNamePrefix + "eleB_Had1Iso",  "eleB Had1Iso distribution (" + str_eleType_GSFeles_cutsPhrase + "); eleB #Sigma E_{T}^{Had1};" + str_NoOfEles, 200, -10.0, +10.0);
+		eleAHist_modHad1Iso_ = new TH1D(hNamePrefix + "eleA_modHad1Iso",  "eleA modHad1Iso distribution (" + str_eleType_GSFeles_cutsPhrase + "); eleA #Sigma E_{T}^{modHad1};" + str_NoOfEles, 200, -10.0, +10.0);
+		eleBHist_modHad1Iso_ = new TH1D(hNamePrefix + "eleB_modHad1Iso",  "eleB modHad1Iso distribution (" + str_eleType_GSFeles_cutsPhrase + "); eleB #Sigma E_{T}^{modHad1};" + str_NoOfEles, 200, -10.0, +10.0);
+		eleAHist_modHad1IsoOverHad1_ = new TH1D(hNamePrefix + "eleA_modHad1IsoOverHad1",  "eleA modHad1IsoOverHad1 distribution (" + str_eleType_GSFeles_cutsPhrase + "); eleA #Sigma E_{T}^{modHad1} / #Sigma E_{T}^{Had1};" + str_NoOfEles, 200, -4.5, +1.5);
+		eleBHist_modHad1IsoOverHad1_ = new TH1D(hNamePrefix + "eleB_modHad1IsoOverHad1",  "eleB modHad1IsoOverHad1 distribution (" + str_eleType_GSFeles_cutsPhrase + "); eleB #Sigma E_{T}^{modHad1} / #Sigma E_{T}^{Had1};" + str_NoOfEles, 200, -4.5, +1.5);
+
 		SetHistAttributes(lineColorIdx, lineStyleIdx);
 	}
 
@@ -131,8 +162,11 @@ namespace tsw{
 		delete diEleHist_deltaEta_;
 		delete diEleHist_deltaPhi_;
 		delete diEleHist_deltaR_;
+		delete diEleHist_EtAminEtB_;
+		delete diEleHist_EtAOverEtB_;
 		delete diEleHist_modTrkIso_;
 		delete diEleHist_dRleq03modTrkIso_;
+		delete diEleHist_dRleq03modTrkIsoNoCtfTrk_;
 
 		delete eleAHist_et_;
 		delete eleAHist_energy_;
@@ -141,6 +175,12 @@ namespace tsw{
 		delete eleAHist_modTrkIso_;
 		delete eleAHist_dRleq03modTrkIso_;
 		delete eleAHist_dRleq03modTrkIsoNoCtfTrk_;
+		delete eleAHist_modEmHad1Iso_;
+		delete eleAHist_modEmHad1IsoOverCutThr_;
+		delete eleAHist_modHad1IsoOverModEmHad1_;
+		delete eleAHist_Had1Iso_;
+		delete eleAHist_modHad1Iso_;
+		delete eleAHist_modHad1IsoOverHad1_;
 
 		delete eleBHist_et_;
 		delete eleBHist_energy_;
@@ -149,11 +189,17 @@ namespace tsw{
 		delete eleBHist_modTrkIso_;
 		delete eleBHist_dRleq03modTrkIso_;
 		delete eleBHist_dRleq03modTrkIsoNoCtfTrk_;
+		delete eleBHist_modEmHad1Iso_;
+		delete eleBHist_modEmHad1IsoOverCutThr_;
+		delete eleBHist_modHad1IsoOverModEmHad1_;
+		delete eleBHist_Had1Iso_;
+		delete eleBHist_modHad1Iso_;
+		delete eleBHist_modHad1IsoOverHad1_;
 	}
 
 	/////////////////////////////////
 	// Method to fill histograms ...
-	void DiEleDistns::FillHistos(tsw::HEEPDiEle theDiEle, const Double_t dieleWeight){
+	void DiEleDistns::FillHistos(tsw::HEEPDiEle theDiEle, const Double_t dieleWeight, const bool fillIsolVarHistos = false){
 		//Fill the di-ele histos ...
 		if( theDiEle.isEBEB() )
 			diEleHist_regions_->Fill(0.0, dieleWeight);
@@ -171,6 +217,9 @@ namespace tsw{
 		diEleHist_deltaPhi_->Fill(     theDiEle.deltaPhi()     , dieleWeight);
 		diEleHist_deltaR_->Fill(       theDiEle.deltaR()       , dieleWeight);
 
+		diEleHist_EtAminEtB_->Fill(  (theDiEle.eleA().et()-theDiEle.eleB().et()), dieleWeight );
+		diEleHist_EtAOverEtB_->Fill( (theDiEle.eleA().et()/theDiEle.eleB().et()), dieleWeight );
+
 		//Fill the component ele histos ...
 		eleAHist_et_->Fill(     theDiEle.eleA().et()     , dieleWeight);
 		eleAHist_energy_->Fill( theDiEle.eleA().energy() , dieleWeight);
@@ -181,22 +230,41 @@ namespace tsw{
 		eleBHist_eta_->Fill(    theDiEle.eleB().eta()    , dieleWeight);
 		eleBHist_phi_->Fill(    theDiEle.eleB().phi()    , dieleWeight);
 
-		//Filling the tracker isolation histos ...
-		diEleHist_modTrkIso_->Fill( (theDiEle.eleA_modTrkIso()+theDiEle.eleB_modTrkIso()), dieleWeight);
-		eleAHist_modTrkIso_->Fill( theDiEle.eleA_modTrkIso(), dieleWeight);
-		eleBHist_modTrkIso_->Fill( theDiEle.eleB_modTrkIso(), dieleWeight);
-		if( theDiEle.deltaR()<0.3 ){
-			diEleHist_dRleq03modTrkIso_->Fill( (theDiEle.eleA_modTrkIso()+theDiEle.eleB_modTrkIso()), dieleWeight);
-			eleAHist_dRleq03modTrkIso_->Fill( theDiEle.eleA_modTrkIso(), dieleWeight);
-			eleBHist_dRleq03modTrkIso_->Fill( theDiEle.eleB_modTrkIso(), dieleWeight);
-			if( (!(theDiEle.eleA().closestCtfTrk_exists())) || (!(theDiEle.eleB().closestCtfTrk_exists())) )
-				diEleHist_dRleq03modTrkIsoNoCtfTrk_->Fill( (theDiEle.eleA_modTrkIso()+theDiEle.eleB_modTrkIso()), dieleWeight);
-			if( !(theDiEle.eleB().closestCtfTrk_exists()) )
-				eleAHist_dRleq03modTrkIsoNoCtfTrk_->Fill( theDiEle.eleA_modTrkIso(), dieleWeight);
-			if( !(theDiEle.eleA().closestCtfTrk_exists()) )
-				eleBHist_dRleq03modTrkIsoNoCtfTrk_->Fill( theDiEle.eleB_modTrkIso(), dieleWeight);
-		}
+		if(fillIsolVarHistos){
+			//Filling the tracker isolation histos ...
+			diEleHist_modTrkIso_->Fill( (theDiEle.eleA_modTrkIso()+theDiEle.eleB_modTrkIso()), dieleWeight);
+			eleAHist_modTrkIso_->Fill( theDiEle.eleA_modTrkIso(), dieleWeight);
+			eleBHist_modTrkIso_->Fill( theDiEle.eleB_modTrkIso(), dieleWeight);
+			if( theDiEle.deltaR()<0.3 ){
+				diEleHist_dRleq03modTrkIso_->Fill( (theDiEle.eleA_modTrkIso()+theDiEle.eleB_modTrkIso()), dieleWeight);
+				eleAHist_dRleq03modTrkIso_->Fill( theDiEle.eleA_modTrkIso(), dieleWeight);
+				eleBHist_dRleq03modTrkIso_->Fill( theDiEle.eleB_modTrkIso(), dieleWeight);
+				if( (!(theDiEle.eleA().closestCtfTrk_exists())) || (!(theDiEle.eleB().closestCtfTrk_exists())) )
+					diEleHist_dRleq03modTrkIsoNoCtfTrk_->Fill( (theDiEle.eleA_modTrkIso()+theDiEle.eleB_modTrkIso()), dieleWeight);
+				if( !(theDiEle.eleB().closestCtfTrk_exists()) )
+					eleAHist_dRleq03modTrkIsoNoCtfTrk_->Fill( theDiEle.eleA_modTrkIso(), dieleWeight);
+				if( !(theDiEle.eleA().closestCtfTrk_exists()) )
+					eleBHist_dRleq03modTrkIsoNoCtfTrk_->Fill( theDiEle.eleB_modTrkIso(), dieleWeight);
+			}
 
+			// Filling EmHad1 isolation histos ...
+			eleAHist_modEmHad1Iso_->Fill(theDiEle.eleA_modEmHad1Iso(), dieleWeight);
+			eleAHist_modEmHad1IsoOverCutThr_->Fill( (theDiEle.eleA_modEmHad1Iso()/(2.0+0.03*theDiEle.eleA().et())), dieleWeight);
+			//eleAHist_modHad1IsoOverModEmHad1_->Fill(  (theDiEle.eleA().isolHadDepth1()/theDiEle.eleA_modEmHad1Iso()), dieleWeight);
+			eleAHist_modHad1IsoOverModEmHad1_->Fill(  (theDiEle.eleA_modHad1Iso()/theDiEle.eleA_modEmHad1Iso()), dieleWeight);
+			eleBHist_modEmHad1Iso_->Fill(theDiEle.eleB_modEmHad1Iso(), dieleWeight);
+			eleBHist_modEmHad1IsoOverCutThr_->Fill( (theDiEle.eleB_modEmHad1Iso()/(2.0+0.03*theDiEle.eleB().et())), dieleWeight);
+			//eleBHist_modHad1IsoOverModEmHad1_->Fill(  (theDiEle.eleB().isolHadDepth1()/theDiEle.eleB_modEmHad1Iso()), dieleWeight);
+			eleBHist_modHad1IsoOverModEmHad1_->Fill(  (theDiEle.eleB_modHad1Iso()/theDiEle.eleB_modEmHad1Iso()), dieleWeight);
+
+			// Filling Had1 isolation histos ...
+			eleAHist_Had1Iso_->Fill(theDiEle.eleA().isolHadDepth1(), dieleWeight);
+			eleBHist_Had1Iso_->Fill(theDiEle.eleB().isolHadDepth1(), dieleWeight);
+			eleAHist_modHad1Iso_->Fill(theDiEle.eleA_modHad1Iso(), dieleWeight);
+			eleBHist_modHad1Iso_->Fill(theDiEle.eleB_modHad1Iso(), dieleWeight);
+			eleAHist_modHad1IsoOverHad1_->Fill( (theDiEle.eleA_modHad1Iso()/theDiEle.eleA().isolHadDepth1()), dieleWeight);
+			eleBHist_modHad1IsoOverHad1_->Fill( (theDiEle.eleB_modHad1Iso()/theDiEle.eleB().isolHadDepth1()), dieleWeight);
+		}
 	}
 
 	void DiEleDistns::SetHistAttributes(Int_t lineCol, Int_t lineStyle){
@@ -226,6 +294,8 @@ namespace tsw{
 		diEleHist_deltaEta_->Write();
 		diEleHist_deltaPhi_->Write();
 		diEleHist_deltaR_->Write();
+		diEleHist_EtAminEtB_->Write();
+		diEleHist_EtAOverEtB_->Write();
 		diEleHist_modTrkIso_->Write();
 		diEleHist_dRleq03modTrkIso_->Write();
 		diEleHist_dRleq03modTrkIsoNoCtfTrk_->Write();
@@ -238,6 +308,12 @@ namespace tsw{
 		eleAHist_modTrkIso_->Write();
 		eleAHist_dRleq03modTrkIso_->Write();
 		eleAHist_dRleq03modTrkIsoNoCtfTrk_->Write();
+		eleAHist_modEmHad1Iso_->Write();
+		eleAHist_modEmHad1IsoOverCutThr_->Write();
+		eleAHist_modHad1IsoOverModEmHad1_->Write();
+		eleAHist_Had1Iso_->Write();
+		eleAHist_modHad1Iso_->Write();
+		eleAHist_modHad1IsoOverHad1_->Write();
 
 		//Write out the eleB_ histos ...
 		eleBHist_et_->Write();
@@ -247,6 +323,12 @@ namespace tsw{
 		eleBHist_modTrkIso_->Write();
 		eleBHist_dRleq03modTrkIso_->Write();
 		eleBHist_dRleq03modTrkIsoNoCtfTrk_->Write();
+		eleBHist_modEmHad1Iso_->Write();
+		eleBHist_modEmHad1IsoOverCutThr_->Write();
+		eleBHist_modHad1IsoOverModEmHad1_->Write();
+		eleBHist_Had1Iso_->Write();
+		eleBHist_modHad1Iso_->Write();
+		eleBHist_modHad1IsoOverHad1_->Write();
 
 		//Move back to the original directory within the output file ...
 		ptr_outFile->cd("..");
@@ -264,6 +346,8 @@ namespace tsw{
 		tmpPtrsVector.push_back(diEleHist_deltaEta_);
 		tmpPtrsVector.push_back(diEleHist_deltaPhi_);
 		tmpPtrsVector.push_back(diEleHist_deltaR_);
+		tmpPtrsVector.push_back(diEleHist_EtAminEtB_);
+		tmpPtrsVector.push_back(diEleHist_EtAOverEtB_);
 		tmpPtrsVector.push_back(diEleHist_modTrkIso_);
 		tmpPtrsVector.push_back(diEleHist_dRleq03modTrkIso_);
 		tmpPtrsVector.push_back(diEleHist_dRleq03modTrkIsoNoCtfTrk_);
@@ -275,6 +359,12 @@ namespace tsw{
 		tmpPtrsVector.push_back(eleAHist_modTrkIso_);
 		tmpPtrsVector.push_back(eleAHist_dRleq03modTrkIso_);
 		tmpPtrsVector.push_back(eleAHist_dRleq03modTrkIsoNoCtfTrk_);
+		tmpPtrsVector.push_back(eleAHist_modEmHad1Iso_);
+		tmpPtrsVector.push_back(eleAHist_modEmHad1IsoOverCutThr_);
+		tmpPtrsVector.push_back(eleAHist_modHad1IsoOverModEmHad1_);
+		tmpPtrsVector.push_back(eleAHist_Had1Iso_);
+		tmpPtrsVector.push_back(eleAHist_modHad1Iso_);
+		tmpPtrsVector.push_back(eleAHist_modHad1IsoOverHad1_);
 
 		tmpPtrsVector.push_back(eleBHist_et_);
 		tmpPtrsVector.push_back(eleBHist_energy_);
@@ -283,6 +373,12 @@ namespace tsw{
 		tmpPtrsVector.push_back(eleBHist_modTrkIso_);
 		tmpPtrsVector.push_back(eleBHist_dRleq03modTrkIso_);
 		tmpPtrsVector.push_back(eleBHist_dRleq03modTrkIsoNoCtfTrk_);
+		tmpPtrsVector.push_back(eleBHist_modEmHad1Iso_);
+		tmpPtrsVector.push_back(eleBHist_modEmHad1IsoOverCutThr_);
+		tmpPtrsVector.push_back(eleBHist_modHad1IsoOverModEmHad1_);
+		tmpPtrsVector.push_back(eleBHist_Had1Iso_);
+		tmpPtrsVector.push_back(eleBHist_modHad1Iso_);
+		tmpPtrsVector.push_back(eleBHist_modHad1IsoOverHad1_);
 
 		return tmpPtrsVector;
 	}
