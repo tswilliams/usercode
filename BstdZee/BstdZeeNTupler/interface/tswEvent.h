50a70f10d216697d79a29dda7bf94005f1a75a43
tsw <>
2012-02-06 17:39:39 +0000

    Added members & methods associated with storing the following information: Z boson 4mom at LHE; generator weight (from GenEventInfoProduct); MC pile-up weight & vertices info; num reconstructed vertices; standard & inner-veto-area-modified isolation values calc'd from isoDeposits. Hence ClassDef num increased from 6->8.

--- BstdZee/BstdZeeNTupler/interface/tswEvent.h
+++ BstdZee/BstdZeeNTupler/interface/tswEvent.h
@@ -18,11 +18,21 @@ namespace tsw{
 
 			//Methods to set information ...
 			void SetBasicEventInformation(unsigned int runNumber, unsigned int lumiSection, unsigned int eventNumber);
+			void SetMCGenWeight(float genWeight);
+			void SetLHEZbosonInfo(ROOT::Math::XYZTVector p4_Zboson);
+			void SetMCPUInfo(float nVtxPoissonMean, unsigned int nVertices, std::vector<float> vtxZposns, double puWeight_1D);
+			void SetRecoVtxInfo(unsigned int nRecoVtxs, unsigned int nGoodRecoVtxs);
 			void SetEMuTriggerInfo(const std::string eMuPathName, const bool eMuPathDecision);
+			void AddStdEleInfo_isoDep_std(double ,double ,double );
+			void AddStdEleInfo_isoDep_inrVeto(double, double, double );
 			void AddNormMuon(tsw::MuStruct* theMuon);
 
 			/// Prints out basic event information to screen
 			void PrintBasicEventInformation();
+			void PrintMCGenWeight();
+			void PrintLHEZbosonInfo();
+			void PrintPUVtxInfo();
+			void PrintStdEleInfo_isoDepIsoValues(unsigned int iEle);
 
 		private:
 			//General event information ...
@@ -30,9 +40,31 @@ namespace tsw{
 			unsigned int lumiSec_;
 			unsigned int evtNum_;
 
+			// Generator information from MC ...
+			float mc_genWeight_;
+			ROOT::Math::XYZTVector mcLHE_ZbosonP4_;
+
+			// Pile up information from MC ...
+			float mc_bx0_nPUVtxPoissonMean_;
+			unsigned int mc_bx0_nPUVtx_;
+			std::vector<float> mc_bx0_PUVtxZPosns_;
+			double mc_puWeight_1D_;
+
+			// Reconstructed vertex info ...
+			unsigned int recoVtx_totalNum_;
+			unsigned int recoVtx_numGoodVtxs_;
+
 			// Information about e-mu trigger ...
 			std::string trg_emuPath_name_;
 			bool trg_emuPath_decision_;
+
+			// Information about the standard electrons ...
+			std::vector<Double_t> stdEles_isoDeps_stdTrkIso_;
+			std::vector<Double_t> stdEles_isoDeps_stdEcalIso_;
+			std::vector<Double_t> stdEles_isoDeps_stdHcalD1Iso_;
+			std::vector<Double_t> stdEles_isoDeps_inrVetoModTrkIso_;
+			std::vector<Double_t> stdEles_isoDeps_inrVetoModEcalIso_;
+			std::vector<Double_t> stdEles_isoDeps_inrVetoModHcalD1Iso_;
 
 			//Information about the standard muons ...
 			std::vector<ROOT::Math::XYZTVector> normMuons_p4_;
@@ -66,7 +98,7 @@ namespace tsw{
 			std::vector<Double_t> normMuons_outTrk_phi_;
 			std::vector<Int_t>    normMuons_outTrk_charge_;
 
-			ClassDef(tsw::Event,6);
+			ClassDef(tsw::Event,8);
 	};
 }
 
--- BstdZee/BstdZeeNTupler/src/tswEvent.cc
+++ BstdZee/BstdZeeNTupler/src/tswEvent.cc
@@ -6,6 +6,9 @@ namespace tsw{
 
 	Event::Event() :
 		runNum_(0), lumiSec_(0), evtNum_(0),
+		mc_genWeight_(-999.9), mcLHE_ZbosonP4_(),
+		mc_bx0_nPUVtxPoissonMean_(-999.9), mc_bx0_nPUVtx_(9999), mc_bx0_PUVtxZPosns_(), mc_puWeight_1D_(-999.9),
+		recoVtx_totalNum_(9999), recoVtx_numGoodVtxs_(9999),
 		normMuons_charge_(), normMuons_isGlobalMuon_(), normMuons_isTrackerMuon_(), normMuons_isStandAloneMuon_(), normMuons_numMatchedMuonStns_(), normMuons_isolR03_sumPt_(),
 		normMuons_globTrk_exists_(), normMuons_globTrk_pT_(), normMuons_globTrk_eta_(), normMuons_globTrk_phi_(), normMuons_globTrk_charge_(), normMuons_globTrk_numberOfValidMuonHits_(), normMuons_globTrk_normalisedChi2_(),
 		normMuons_inTrk_exists_(), normMuons_inTrk_pT_(), normMuons_inTrk_eta_(), normMuons_inTrk_phi_(), normMuons_inTrk_charge_(), normMuons_inTrk_numValidPixHits_(), normMuons_inTrk_numValidTrkrHits_(), normMuons_inTrk_dxyVsOrigin_(),
@@ -15,13 +18,51 @@ namespace tsw{
 	}
 	Event::~Event() {}
 
-	void Event::SetBasicEventInformation(unsigned int runNumber, unsigned int lumiSection, unsigned int eventNumber){
+	void Event::SetBasicEventInformation(unsigned int runNumber, unsigned int lumiSection, unsigned int eventNumber)
+	{
 		runNum_  = runNumber;
 		lumiSec_ = lumiSection;
 		evtNum_  = eventNumber;
 	}
+	void Event::SetMCGenWeight(float genWeight){
+		mc_genWeight_ = genWeight;
+	}
+	void Event::SetLHEZbosonInfo(ROOT::Math::XYZTVector p4_Zboson){
+		mcLHE_ZbosonP4_ = p4_Zboson;
+	}
+	void Event::SetMCPUInfo(float nVtxPoissonMean, unsigned int nVertices, std::vector<float> vtxZposns, double puWeight_1D)
+	{
+		mc_bx0_nPUVtxPoissonMean_ = nVtxPoissonMean;
+		mc_bx0_nPUVtx_ = nVertices;
+		mc_bx0_PUVtxZPosns_ = vtxZposns;
+		mc_puWeight_1D_ = puWeight_1D;
+	}
+	void Event::SetRecoVtxInfo(unsigned int nRecoVtxs, unsigned int nGoodRecoVtxs)
+	{
+		recoVtx_totalNum_ = nRecoVtxs;
+		recoVtx_numGoodVtxs_ = nGoodRecoVtxs;
+	}
+
 	void Event::PrintBasicEventInformation(){
 		std::cout << "Run " << runNum_ << ", LumiSec " << lumiSec_ << ", event " << evtNum_ << std::endl;
+	}
+	void Event::PrintMCGenWeight()
+	{
+		std::cout << "  * MC gen weight = " << mc_genWeight_ << std::endl;
+	}
+	void Event::PrintLHEZbosonInfo(){
+		std::cout << "  * LHE info:" << std::endl;
+		std::cout << "       Z boson p4(px,py,pz,E) = " << "(" << mcLHE_ZbosonP4_.Px() << ", " <<  mcLHE_ZbosonP4_.Py() << ", " <<  mcLHE_ZbosonP4_.Pz() << ", " <<  mcLHE_ZbosonP4_.E() << "); pT=" << mcLHE_ZbosonP4_.Pt() << std::endl;
+	}
+	void Event::PrintPUVtxInfo(){
+		std::cout << "  * MC PU Info ... " << std::endl;
+		std::cout << "       bx0: true num PU vtxs = " << mc_bx0_nPUVtxPoissonMean_ << "; num PU vtxs = " << mc_bx0_nPUVtx_ << "; puWeight=" << mc_puWeight_1D_ << std::endl;
+		std::cout << "            z positions: ";
+		for(unsigned int iVtx=0; iVtx<mc_bx0_PUVtxZPosns_.size(); iVtx++)
+			std::cout << mc_bx0_PUVtxZPosns_.at(iVtx) << ", ";
+		std::cout << std::endl;
+		std::cout << "  * Reconstructed vertices:" << std::endl;
+		std::cout << "       totNum=" << recoVtx_totalNum_ << "; numGoodVtxs=" << recoVtx_numGoodVtxs_ << std::endl;
 	}
 
 	void Event::SetEMuTriggerInfo(const std::string eMuPathName, const bool eMuPathDecision)
@@ -29,6 +70,26 @@ namespace tsw{
 		trg_emuPath_name_ = eMuPathName;
 		trg_emuPath_decision_ = eMuPathDecision;
 	}
+
+	void Event::AddStdEleInfo_isoDep_std(const double stdTrkIso, const double stdEcalIso, const double stdHcalD1Iso)
+	{
+		stdEles_isoDeps_stdTrkIso_.push_back( stdTrkIso );
+		stdEles_isoDeps_stdEcalIso_.push_back( stdEcalIso );
+		stdEles_isoDeps_stdHcalD1Iso_.push_back( stdHcalD1Iso );
+	}
+	void Event::AddStdEleInfo_isoDep_inrVeto(const double modTrkIso, const double modEcalIso, const double modHcalD1Iso)
+	{
+		stdEles_isoDeps_inrVetoModTrkIso_.push_back( modTrkIso );
+		stdEles_isoDeps_inrVetoModEcalIso_.push_back( modEcalIso );
+		stdEles_isoDeps_inrVetoModHcalD1Iso_.push_back( modHcalD1Iso );
+	}
+	void Event::PrintStdEleInfo_isoDepIsoValues(unsigned int iEle){
+		std::cout << "    Iso values from isoDeposits... " << std::endl;
+		std::cout << "          Std values:    Trk=" << stdEles_isoDeps_stdTrkIso_.at(iEle) << ", Ecal=" << stdEles_isoDeps_stdEcalIso_.at(iEle) << ", HcalDepth1=" << stdEles_isoDeps_stdHcalD1Iso_.at(iEle) << std::endl;
+		std::cout << "          Mod (inrVeto): Trk=" << stdEles_isoDeps_inrVetoModTrkIso_.at(iEle) << ", Ecal=" << stdEles_isoDeps_inrVetoModEcalIso_.at(iEle) << ", HcalDepth1=" << stdEles_isoDeps_inrVetoModHcalD1Iso_.at(iEle) << std::endl;
+
+	}
+
 
 	void Event::AddNormMuon(tsw::MuStruct* theMuon){
 		normMuons_p4_                .push_back(theMuon->p4);
