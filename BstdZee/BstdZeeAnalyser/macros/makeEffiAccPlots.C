42df094a1e34aaea6aaef4e1ed5c64f1ff7317b3
tsw <>
2013-02-21 12:34:11 +0000

    Update to latest set of plots in note.

--- BstdZee/BstdZeeAnalyser/macros/makeEffiAccPlots.C
+++ BstdZee/BstdZeeAnalyser/macros/makeEffiAccPlots.C
@@ -1,8 +1,9 @@
 {
 	tsw::Samples2012 anaTuples("zEffiTree");
+	std::string outFileDir = "results/20130212/ModIsoMcEffi/";
 
 	//-> 0) Standard cuts applied to EffiCalcTree ...
-	std::string seln_accept                 = "( (eleA_p4.Pt()>35.0 && abs(eleA_p4.Eta())<1.44 ) && (eleB_p4.Pt()>35.0 && abs(eleB_p4.Eta())<1.44) && (Zp4.M()>60.0 && Zp4.M()<120.0) )";
+ 	std::string seln_accept                 = "( (eleA_p4.Pt()>35.0 && abs(eleA_p4.Eta())<1.44 ) && (eleB_p4.Pt()>35.0 && abs(eleB_p4.Eta())<1.44) && (Zp4.M()>60.0 && Zp4.M()<120.0) )";
 	std::string seln_reco                   = "bothRecod";
 	std::string seln_MPhiVeto               = "(abs(ZdEta)>0.07 || abs(ZdPhi)>0.3)";
 	std::string seln_acceptRecoAndPhiVeto  = tsw::AndOfCuts( tsw::AndOfCuts(seln_accept, seln_reco), seln_MPhiVeto);
@@ -26,7 +27,7 @@
 	tsw::EffiDefn relEffi_reco(seln_accept, seln_reco, "reco", tsw::Black);
 	tsw::EffiDefn relEffi_MPhiVeto(relEffi_reco.denomAndNumerCuts(), seln_MPhiVeto, "#phi road", tsw::Pink);
 	tsw::EffiDefn relEffi_heepId(seln_acceptRecoAndPhiVeto, seln_heepId, "HEEP ID", tsw::Blue);
-	tsw::EffiDefn relEffi_stdHeep(seln_acceptRecoPhiVetoAndId, seln_stdHeep, "Both standard isol", tsw::Red);
+	tsw::EffiDefn relEffi_stdHeepIso(seln_acceptRecoPhiVetoAndId, seln_stdHeep, "Both standard isol", tsw::Red);
 	tsw::EffiDefn relEffi_stdHeepTrk(seln_acceptRecoPhiVetoAndId, seln_stdHeepTrk, "Standard I_{trk}", tsw::Black);
 	tsw::EffiDefn relEffi_stdHeepCalo(seln_acceptRecoPhiVetoAndId, seln_stdHeepCalo, "Standard I_{calo}", tsw::Black);
 	//
@@ -45,7 +46,7 @@
 	tsw::EffiDefn absEffi_reco("1", relEffi_reco.denomAndNumerCuts(), "reco", tsw::Black);
 	tsw::EffiDefn absEffi_MPhiVeto("1", relEffi_MPhiVeto.denomAndNumerCuts(), "#phi road", tsw::Pink);
 	tsw::EffiDefn absEffi_heepId("1", relEffi_heepId.denomAndNumerCuts(), "heepId", tsw::Blue);
-	tsw::EffiDefn absEffi_stdHeep("1", relEffi_stdHeep.denomAndNumerCuts(), "StdIso", tsw::Red);
+	tsw::EffiDefn absEffi_stdHeep("1", relEffi_stdHeepIso.denomAndNumerCuts(), "StdIso", tsw::Red);
 	tsw::EffiDefn absEffi_modHeepStdThr("1", relEffi_modHeepStdThr.denomAndNumerCuts(), "ModIso (std thr)", tsw::Green);
 	tsw::EffiDefn absEffi_modHeepColThr("1", relEffi_modHeepColThr.denomAndNumerCuts(), "ModIso (col thr)", tsw::LightBlue);
 
@@ -80,22 +81,21 @@
 ////			.add( absEffi_modHeepStdThr )
 //	accPlotter//.add( relEffi_modHeepColThr ).add( relEffi_modHeepStdThr )
 //			.add( relEffi_genHadPtSumLeq5 )
-//			.outFilePrefix("results/20121203/signalMc_isolEffiVsMass").run();
+//			.outFilePrefix("results/20121213/signalMc_isolEffiVsMass").run();
 
 
 	//----------------------------//
 	// --- MOD ISO EFFI PLOTS --- //
 	//----------------------------//
 
-	tsw::AxisDefn zPt_calchep("Zp4.Pt()", 30, 280.0, 1480.0, "p_{T,ee} [GeV]");
-	tsw::AxisDefn zE_calchep("Zp4.E()", 28, 280.0, 2520.0, "Z boson energy, E_{ee} [GeV]");
-	tsw::AxisDefn zMom_calchep("Zp4.P()", 30, 280.0, 1480.0, "p_{ee} [GeV]");
+	tsw::AxisDefn zPt_calchep("Zp4.Pt()", "280,320,360,400,440,480,520,560,600,640,680,720,760,800,840,880,920,960,1000,1040,1080,1120,1160,1200,1300,1400,1500", "p_{T,ee} [GeV]");
+	tsw::AxisDefn zMom_calchep("Zp4.P()", "280,360,440,520,600,680,760,840,920,1000,1080,1160,1240,1320,1400,1500,1600,1700,1800,2000,2250,2500", "Z boson momentum, p_{ee} [GeV]");
+	//tsw::AxisDefn zMom_calchep("Zp4.P()", 30, 280.0, 1480.0, "p_{ee} [GeV]");
 	tsw::AxisDefn dRee_calchep("ZdR", 15, 0.1, 0.7, "#Delta R_{ee}");
-	double zPtLims_mg[] = {280., 320., 360., 400., 440., 480., 520., 600., 700., 1000.};
-	double dRLims_mg[] = {.1, .3, .34, .38, .42, .44, .46, .48, .5, .52, .54, .56, .58, .6, .62, .64, .66, .68, .7};
-	tsw::AxisDefn zPt_mg("Zp4.E()", 9, zPtLims_mg, "Z boson energy, E_{ee} [GeV]");
-	tsw::AxisDefn zMom_mg("Zp4.P()", 9, zPtLims_mg, "p_{ee} [GeV]");
-	tsw::AxisDefn dRee_mg("ZdR", 18, dRLims_mg, "#Delta R_{ee}");
+
+	tsw::AxisDefn zPt_mg("Zp4.E()", "280, 320, 360, 400, 440, 480, 520, 600, 700, 1000", "p_{T,ee}, E_{ee} [GeV]");
+	tsw::AxisDefn zMom_mg("Zp4.P()", "280, 320, 360, 400, 440, 480, 520, 600, 700, 1000", "p_{ee} [GeV]");
+	tsw::AxisDefn dRee_mg("ZdR", "0.1, 0.3, 0.34, 0.38, 0.42, 0.44, 0.46, 0.48, 0.5, 0.52, 0.54, 0.56, 0.58, 0.6, 0.62, 0.64, 0.66, 0.68, 0.7", "#Delta R_{ee}");
 
 
 	// Effis
@@ -103,51 +103,61 @@
 	effiPlotter_recoAndId_b4PhiVeto//.add( dyEe_calchep ).add( zPt_calchep ).add( dRee_calchep )
 		.add( tsw::EffiDefn(seln_accept, seln_reco, "RECO", tsw::Black) )
 		.add( tsw::EffiDefn(seln_accept, tsw::AndOfCuts(seln_heepId, seln_reco), "RECO + HEEP ID", tsw::Blue) )
-		.gridLinesY().descriptiveText("Within acceptance").outFilePrefix("results/20121203/effi/effiAfterAcc_RecoId");
+		.gridLinesY().descriptiveText("Within acceptance").outFilePrefix(outFileDir+"/effiAfterAcc_RecoId");
 	effiPlotter_recoAndId_b4PhiVeto
-	.drawPlot( anaTuples.dyEE_calchep(), zPt_calchep ).drawPlot( anaTuples.dyEE_calchep(), zE_calchep ).drawPlot( anaTuples.dyEE_calchep(), dRee_calchep );
+	.drawPlot( anaTuples.dyEE_calchep(), zPt_calchep ).drawPlot( anaTuples.dyEE_calchep(), zMom_calchep ).drawPlot( anaTuples.dyEE_calchep(), dRee_calchep );
 
 	tsw::EffiPlotter effiPlotter_recoAndId_b4PhiVetoSmallDr("zBosonEffiTree", 0.0);
 	effiPlotter_recoAndId_b4PhiVetoSmallDr
 			.add( tsw::EffiDefn(tsw::AndOfCuts(seln_accept, "ZdR<0.3"), seln_reco, "RECO", tsw::Black) )
 			.add( tsw::EffiDefn(tsw::AndOfCuts(seln_accept, "ZdR<0.3"), tsw::AndOfCuts(seln_heepId, seln_reco), "RECO + HEEP ID", tsw::Blue) )
 //			.add( tsw::EffiDefn(tsw::AndOfCuts(tsw::AndOfCuts(seln_accept, "ZdR<0.3"),seln_reco), seln_heepId, "HEEP ID wrt RECO", tsw::Pink) )
-			.gridLinesY().descriptiveText("Within acceptance, #Delta R < 0.3").outFilePrefix("results/20121203/effi/effiAfterAccDrLeThan03_RecoId");
+			.gridLinesY().descriptiveText("Within acceptance, #Delta R < 0.3").outFilePrefix(outFileDir+"/effiAfterAccDrLeThan03_RecoId");
 	effiPlotter_recoAndId_b4PhiVetoSmallDr
-			.drawPlot( anaTuples.dyEE_calchep(), zPt_calchep ).drawPlot( anaTuples.dyEE_calchep(), zE_calchep ).drawPlot( anaTuples.dyEE_calchep(), dRee_calchep )
+			.drawPlot( anaTuples.dyEE_calchep(), zPt_calchep ).drawPlot( anaTuples.dyEE_calchep(), zMom_calchep ).drawPlot( anaTuples.dyEE_calchep(), dRee_calchep )
 			.drawPlot( anaTuples.dyEE_calchep(), tsw::AxisDefn("abs(ZdEta)", 14, 0.0, 0.35, "#Delta#eta_{ee}") );
 
 	tsw::EffiPlotter effiPlotter_recoAndPhiRdVeto("zBosonEffiTree", 0.0);
 	effiPlotter_recoAndPhiRdVeto
 			.add( tsw::EffiDefn(seln_accept, seln_reco, "RECO", tsw::Black) )
 			.add( tsw::EffiDefn(seln_accept, seln_acceptRecoAndPhiVeto, "RECO + #phi rd veto", tsw::Pink) )
-			.gridLinesY().descriptiveText("Within acceptance").outFilePrefix("results/20121203/effi/effiAfterAcc_RecoPhiRoads");
-	effiPlotter_recoAndPhiRdVeto.drawPlot( anaTuples.dyEE_calchep(), zPt_calchep ).drawPlot( anaTuples.dyEE_calchep(), zE_calchep )
+			.gridLinesY().descriptiveText("Within acceptance").outFilePrefix(outFileDir+"/effiAfterAcc_RecoPhiRoads");
+	effiPlotter_recoAndPhiRdVeto.drawPlot( anaTuples.dyEE_calchep(), zPt_calchep ).drawPlot( anaTuples.dyEE_calchep(), zMom_calchep )
 			.drawPlot( anaTuples.dyEE_calchep(), dRee_calchep );
 
 	tsw::EffiPlotter effiPlotter_IdAndStdIsosRelToRecoWPhiVeto("zBosonEffiTree", 0.0);
 	effiPlotter_IdAndStdIsosRelToRecoWPhiVeto.gridLinesY().legendPosition(tsw::LOWER_LEFT)
+			.add( tsw::EffiDefn(tsw::AndOfCuts(seln_accept,seln_MPhiVeto), seln_reco, "RECO", tsw::Black ) )
 			.add( relEffi_heepId ).add( tsw::EffiDefn(seln_acceptRecoAndPhiVeto, tsw::AndOfCuts(seln_heepId,seln_stdHeepTrk), "ID + track iso", tsw::Red) )
 			.add( tsw::EffiDefn(seln_acceptRecoAndPhiVeto, tsw::AndOfCuts(seln_heepId,seln_stdHeepCalo), "ID + cal iso", tsw::Green) )
-			.descriptiveText("Recontructed ee pairs after #phi road veto").outFilePrefix("results/20121203/effi/effiAfterAccRecoPhiRd_IdStdIso");
-	effiPlotter_IdAndStdIsosRelToRecoWPhiVeto.drawPlot( anaTuples.dyEE_calchep(), zPt_calchep ).drawPlot( anaTuples.dyEE_calchep(), zE_calchep )
+			.descriptiveText("ee pairs after #phi road veto").outFilePrefix(outFileDir+"/effiAfterAccRecoPhiRd_IdStdIso");
+	effiPlotter_IdAndStdIsosRelToRecoWPhiVeto.drawPlot( anaTuples.dyEE_calchep(), zPt_calchep ).drawPlot( anaTuples.dyEE_calchep(), zMom_calchep )
 			.drawPlot( anaTuples.dyEE_calchep(), dRee_calchep );
 
 
 	tsw::EffiPlotter effiPlotter_modTrkIso("zBosonEffiTree", 0.7);
 	tsw::EffiPlotter effiPlotter_modCalIso(effiPlotter_modTrkIso);
 	effiPlotter_modTrkIso.add( relEffi_stdHeepTrk ).add( relEffi_modHeepTrkStdThr )
-			.gridLinesY().outFilePrefix("results/20121203/effi/modTrkIsoEffiAfterId");
+			.gridLinesY().outFilePrefix(outFileDir+"/modTrkIsoEffiAfterId");
 	effiPlotter_modTrkIso
-			.drawPlot( anaTuples.dyEE_calchep(), zPt_calchep ).drawPlot( anaTuples.dyEE_calchep(), zE_calchep ).drawPlot( anaTuples.dyEE_calchep(), dRee_calchep )
+			.drawPlot( anaTuples.dyEE_calchep(), zPt_calchep ).drawPlot( anaTuples.dyEE_calchep(), zMom_calchep ).drawPlot( anaTuples.dyEE_calchep(), dRee_calchep )
 			;/*.drawPlot( anaTuples.dyEE_mg_merged(), zPt_mg )   .drawPlot( anaTuples.dyEE_mg_merged(), dRee_mg );*/
 
 	effiPlotter_modCalIso.add( relEffi_modHeepCaloStdThr_SC ).add( relEffi_modHeepCaloStdThr_5xtal )
 			.add( relEffi_modHeepCaloStdThr_4xtal ).add( relEffi_modHeepCaloStdThr_3xtal )
-			.gridLinesY().legendPosition(tsw::LOWER_LEFT).outFilePrefix("results/20121203/effi/modCalIsoEffiAfterId");
+			.gridLinesY().legendPosition(tsw::LOWER_LEFT).outFilePrefix(outFileDir+"/modCalIsoEffiAfterId");
 	effiPlotter_modCalIso
-			.drawPlot( anaTuples.dyEE_calchep(), zPt_calchep ).drawPlot( anaTuples.dyEE_calchep(), zE_calchep ).drawPlot( anaTuples.dyEE_calchep(), dRee_calchep )
+			.drawPlot( anaTuples.dyEE_calchep(), zPt_calchep ).drawPlot( anaTuples.dyEE_calchep(), zMom_calchep ).drawPlot( anaTuples.dyEE_calchep(), dRee_calchep )
 			;/*.drawPlot( anaTuples.dyEE_mg_merged(), zPt_mg )   .drawPlot( anaTuples.dyEE_mg_merged(), dRee_mg );*/
+
+   tsw::EffiPlotter effiPlotter_summary("zBosonEffiTree", 0.0);
+   effiPlotter_summary.add( tsw::EffiDefn(seln_acceptRecoAndPhiVeto, seln_heepId, "HEEP ID (no iso)", tsw::Blue ) )
+      .add( tsw::EffiDefn(seln_acceptRecoAndPhiVeto, seln_stdHeep, "ID + std iso", tsw::Red ) )
+      .add( tsw::EffiDefn(seln_acceptRecoAndPhiVeto, seln_modHeepStdThr, "ID + mod iso", tsw::Green ) )
+      .gridLinesY().legendPosition(tsw::LOWER_LEFT).outFilePrefix(outFileDir+"/stdIsoVsModIso");
+   effiPlotter_summary.drawPlot( anaTuples.dyEE_calchep(), zPt_calchep ).drawPlot( anaTuples.dyEE_calchep(), zMom_calchep )
+      .legendPosition(tsw::LOWER_RIGHT).drawPlot( anaTuples.dyEE_calchep(), dRee_calchep );
+	
 
 //	tsw::EffiPlotter effiPlotter_chep("zBosonEffiTree", 0.7);
 //	tsw::EffiPlotter effiPlotter_mg(effiPlotter_chep);
@@ -158,16 +168,16 @@
 //	tsw::EffiPlotter effiPlotter_mg2( effiPlotter_mg );
 //
 //	effiPlotter_chep.add( relEffi_stdHeepTrk  ).add( relEffi_modHeepTrkStdThr )
-//			.outFilePrefix("results/20121203/accEffi/modTrkIsoEffiDyCalcHEP_").run();
+//			.outFilePrefix("results/20121213/accEffi/modTrkIsoEffiDyCalcHEP_").run();
 //	effiPlotter_mg.add( relEffi_stdHeepTrk  ).add( relEffi_modHeepTrkStdThr )
-//			.outFilePrefix("results/20121203/accEffi/modTrkIsoEffiDyMG_").run();
+//			.outFilePrefix("results/20121213/accEffi/modTrkIsoEffiDyMG_").run();
 //	effiPlotter_chep2
 //			.add( relEffi_modHeepCaloStdThr_SC ).add( relEffi_modHeepCaloStdThr_5xtal )
 //			.add( relEffi_modHeepCaloStdThr_4xtal ).add( relEffi_modHeepCaloStdThr_3xtal )
-//			.outFilePrefix("results/20121203/accEffi/modCaloIsoEffiDyCalcHEP_").run();
+//			.outFilePrefix("results/20121213/accEffi/modCaloIsoEffiDyCalcHEP_").run();
 //	effiPlotter_mg2
 //			.add( relEffi_modHeepCaloStdThr_SC ).add( relEffi_modHeepCaloStdThr_5xtal )
 //			.add( relEffi_modHeepCaloStdThr_4xtal ).add( relEffi_modHeepCaloStdThr_3xtal )
-//			.outFilePrefix("results/20121203/accEffi/modCaloIsoEffiDyMG_").run();
+//			.outFilePrefix("results/20121213/accEffi/modCaloIsoEffiDyMG_").run();
 
 }
